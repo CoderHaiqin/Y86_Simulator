e53a2f209bf4c5ae884e3d8cc1f9dcde078c5483
haiqin <2410800314@qq.com>
2023-03-16 14:49:24 +0800

    format

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include "simulator.h"
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-	Simulator s;
-	s.load("D:\\test.txt");
-	std::string str;
-	while(1){
-		if(s.W.stat != AOK){
-			std::cout << "end" << std::endl;
-			std::getline(cin, str);
-		}
-		s.tick();
-	}
+int main(int argc, char *argv[]) {
+  Simulator s;
+  s.load("D:\\test.txt");
+  std::string str;
+  while (1) {
+    if (s.W.stat != AOK) {
+      std::cout << "end" << std::endl;
+      std::getline(cin, str);
+    }
+    s.tick();
+  }
 }
--- regisfile.h
+++ regisfile.h
@@ -1,16 +1,16 @@
 #ifndef REGISFILE_H
 #define REGISFILE_H
-#include "constant.h"
 #include <iostream>
 
-class RegisFile
-{
-public:
-    word data[16];
-    word get(byte src);
-    void set(byte dst, word value);
+#include "constant.h"
 
-    RegisFile();
+class RegisFile {
+ public:
+  word data[16];
+  word get(byte src);
+  void set(byte dst, word value);
+
+  RegisFile();
 };
 
-#endif // REGISFILE_H
+#endif  // REGISFILE_H
